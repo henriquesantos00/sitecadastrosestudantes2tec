01cc68292d9395426de384b0473ad03f94d996ad
Henrique Mariano Santos <henrique.mariano.santos@escola.pr.gov.br>
2026-05-04 18:08:18 +0000

    fe

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ form.addEventListener('submit', function (event) {
 
     let nome = document.getElementById('nomeAluno').value;
     let idade = document.getElementById('idadeAluno').value;
+    let cgm = document.getElementById('cgmAluno').value;
+    let email = document.getElementById('emailAluno').value;
 
     const novoItem = document.createElement('li');
-    novoItem.innerHTML = `<strong>${nome}</strong> - <strong>Idade:</strong> ${idade} anos`;
+    novoItem.innerHTML = `
+        <strong>${nome}</strong> - <strong>Idade:</strong> ${idade} anos
+        - <strong>CGM:</strong> ${cgm} - <strong>Email:</strong> ${email}
+    `;
     lista.appendChild(novoItem);
 
     form.reset();
